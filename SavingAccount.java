cef9de1d5f81446c9854b64e0ba6fc97d4c53f47
Hei-dev <59471608+Hei-dev@users.noreply.github.com>
2023-10-26 20:31:50 +0800

    Update SavingAccount.java from "CurrentAccount"
    lol

--- SavingAccount.java
+++ SavingAccount.java
@@ -1,22 +1,20 @@
-public class CurrentAccount extends Account{
-    private double limitPerCheque = 10000;
+public class SavingAccount extends Account{
+    private double interestRate = 0.1;
     // The constructor, which also calls the parent constructor
-    public CurrentAccount(int theAccountNumber, int thePIN, double theAvailableBalance, double theTotalBalance , double limitPerCheque){
+    public SavingAccount(int theAccountNumber, int thePIN, double theAvailableBalance, double theTotalBalance , double interestRate){
         super(theAccountNumber, thePIN, theAvailableBalance, theTotalBalance);
-        this.limitPerCheque = limitPerCheque;
     }
-    // Setter for limitPerCheque
-    public void setLimitPerCheque(double amt){
-        limitPerCheque = amt;
+    // Getter for interestRate
+    public double getInterestRate(){
+        return interestRate;
     }
     
-    // Getter for limitPerCheque
-    public double getLimitPerCheque(){
-        return limitPerCheque;
+    // Setter for interestRate
+    public void setInterestRate(double amt){
+        interestRate = amt;
     }
-
-    @Override
-    public void saveAccount(double[] dummy){
-        super.saveAccount(new double[]{(double) super.accountNumber, (double) super.pin, super.availableBalance, super.totalBalance, 2.0, limitPerCheque});
+    
+    public void saveAccount(){
+        super.saveAccount()
     }
 }
