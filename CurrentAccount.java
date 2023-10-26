423fcbba044c1130680d730e1980e7999d4f8970
Hei-dev <59471608+Hei-dev@users.noreply.github.com>
2023-10-26 13:06:30 +0800

    AccountManagement fully functional
    Offically Added CurrentAccount

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -1,15 +1,22 @@
 public class CurrentAccount extends Account{
     private double limitPerCheque = 10000;
     // The constructor, which also calls the parent constructor
-    public CurrentAccount(int theAccountNumber, int thePIN, double theAvailableBalance, double theTotalBalance ){
+    public CurrentAccount(int theAccountNumber, int thePIN, double theAvailableBalance, double theTotalBalance , double limitPerCheque){
         super(theAccountNumber, thePIN, theAvailableBalance, theTotalBalance);
+        this.limitPerCheque = limitPerCheque;
     }
     // Setter for limitPerCheque
     public void setLimitPerCheque(double amt){
         limitPerCheque = amt;
     }
+    
     // Getter for limitPerCheque
     public double getLimitPerCheque(){
         return limitPerCheque;
+    }
+
+    @Override
+    public void saveAccount(double[] dummy){
+        super.saveAccount(new double[]{(double) super.accountNumber, (double) super.pin, super.availableBalance, super.totalBalance, 2.0, limitPerCheque});
     }
 }
