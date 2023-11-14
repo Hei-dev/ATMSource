fd0ad80768a0735f2efbbf37e639dfff09d8457a
Hei-dev <59471608+Hei-dev@users.noreply.github.com>
2023-11-14 17:31:20 +0800

    #4

--- MainMenugui.java
+++ MainMenugui.java
@@ -31,10 +31,11 @@ public class MainMenugui extends ATMgui{
 		setSelectionButtonListener(7, new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent evt){
+				System.exit(0);
+
 				ATMgui.get(GUIType.Exit).run();
 
-				javax.swing.JOptionPane.showMessageDialog(null, "Btn clicked");
-				System.exit(0);
+				javax.swing.JOptionPane.showMessageDialog(null, "7 Btn clicked");
 			}
 		});
 
