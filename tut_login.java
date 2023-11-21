2685e3aa6210cdc34650e802da7a2a7606b23efc
Hei-dev <59471608+Hei-dev@users.noreply.github.com>
2023-11-21 13:41:55 +0800

    tut3

--- tut_login.java
+++ tut_login.java
@@ -1,31 +1,56 @@
 
 // start display login ui
 
+// Acc number enter
 ATMgui.get().setEnterListener(new ActionListener(){
     @Override
     public void actionPerformed(ActionEvent ae){
-        boolean userAuthenticated = bankDatabase.authenticateUser( accountNumber, pin );
-      
-      // check whether authentication succeeded
-      if ( userAuthenticated )
-      {
-         currentAccountNumber = accountNumber; // save user's account #
-
-         // proceeed to main menu
-      } 
-      else{
-        JLabel error_message = new JLabel("");
-        error_message.setName("err_msg");
-        login_ui.add(error_message);
-
-        error_message = (JLabel)ATMgui.get().findComponentByName("err_msg");
-
-        login_ui.remove(error_message);
-
-        error_message.setText("Invalid");
-
-        login_ui.add(error_message);
-        ATMgui.get().addMainPanel(login_ui)
-      }
+        //accountNumber = textfield getvalue
+        passwordCheck();
     }
-})
+});
+
+// Password check
+private void passwordCheck(){
+    ATMgui.get().setEnterListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+            boolean userAuthenticated = bankDatabase.authenticateUser( accountNumber, pin );
+            
+            // check whether authentication succeeded
+            if ( userAuthenticated )
+            {
+                currentAccountNumber = accountNumber; // save user's account #
+
+                // proceeed to main menu
+            } 
+            else{
+                JLabel error_message = new JLabel("");
+                error_message.setName("err_msg");
+                login_ui.add(error_message);
+
+                error_message = (JLabel)ATMgui.get().findComponentByName("err_msg");
+
+                login_ui.remove(error_message);
+
+                error_message.setText("Invalid");
+
+                login_ui.add(error_message);
+                ATMgui.get().addMainPanel(login_ui)
+
+
+                ATMgui.get().revalidate();
+                ATMgui.get().repaint();
+
+                ATMgui.get().setEnterListener(new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent evt){
+                        passwordCheck();
+                    }
+                });
+
+            }
+        }
+    })
+}
+
