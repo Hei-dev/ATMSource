790a808382b635e5f64bf31fe5c8cd7aecb1125b
CrazyUOuOb <96434813+CrazyUOuOb@users.noreply.github.com>
2023-11-24 18:12:08 +0800

    finish transfer

--- Transfer.java
+++ Transfer.java
@@ -1,112 +1,206 @@
-// Transfer.java
-// Represents a Transfer ATM transaction
+import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Font;
 
-public class Transfer extends Transaction
+public class TransferGUI implements Defaultgui 
 {
-   private static double amount; // amount to transfer
-   private static int target_account; // target account # to transfer
-   private static int current_account;
-   private static double available_balance;   
-   private static int account_checker; // special checker for detect same account
-   
-   private final static int CANCELED = 0; // constant for cancel option
-   private final static int Decimal_value = -2; // constant for handle non-integer
-   private final static int Insufficient_cash = -3; // constant for not enough money
-   private final static int same_account = -4; // constant for same account number
-   private final static int Invaild_value = -404; // constant for handle invaild value
-   
-
-   // Transfer constructor
-   public Transfer( int userAccountNumber, ATMgui atmScreen, 
-      BankDatabase atmBankDatabase)
-   {
-      // initialize superclass variables
-      super( userAccountNumber, atmScreen, atmBankDatabase );
-   } // end Transfer constructor
-   
-   public double return_balance()
-   {
-       return available_balance;
-   }
+    private JPanel TransferGUI;
     
-   // perform transaction
-   public void execute()
-   {
-      BankDatabase bankDatabase = getBankDatabase(); // get reference
-      ATMgui screen = getScreen(); // get reference
-      
-      current_account = 12345;
-      //current_account = getAccountNumber();
-      
-      available_balance = 1000;
-      //available_balance = bankDatabase.getAvailableBalance( current_account );
-   } // end method promptForTargetAccount
-   
-   public static int check_amount( String input )
-   {    
-        try 
+    private String target_account;
+    private String amount;
+    private int current_account;
+    private Font 
+        transferfont,
+        amountfont;
+    private BankDatabase bankDB;
+    private Transaction transaction;
+    
+    private final static int CANCELED = 0; // constant for cancel option
+    private final static int Decimal_value = -2; // constant for handle non-integer
+    private final static int Insufficient_cash = -3; // constant for not enough money
+    private final static int same_account = -4; // constant for not enough money
+    private final static int Invaild_account = -5; // constant for unexist account number
+    private final static int Invaild_value = -404; // constant for handle invaild value    
+    
+    protected TransferGUI()
+    {
+        TransferGUI = getdefaultGUI();
+        transferfont = new Font("Transfont", 1, 20);
+        amountfont = new Font("Amtfont", 1, 12);
+        
+        target_account = "-1";
+        amount = "-1";
+                    
+        // change title
+        setComponentText
+        (TransferGUI, "Title", "<html>Please enter transfer amount!<br/>(0 to cancel)</html>", transferfont);
+            
+        //disable all selection message
+        for (int i = 0; i < 8; i++)
         {
-            amount = Double.parseDouble( input );
-        } catch (IllegalArgumentException e) {
-            return Invaild_value;
+                setSelectionDisplay(TransferGUI, i, false);
         }
-        if ( amount == CANCELED)
+    }
+    
+    public JPanel getPanel() 
+    {
+        return TransferGUI;
+    }
+    
+    private void execute_amount()
+    {
+        String input = getTextPaneText( TransferGUI );
+        int status = Transfer.check_amount ( input );
+        if (status == CANCELED)
         {
-            return CANCELED;
+            ATMgui.get().display(GUIType.MainMenu);
         }
-        else if ( amount > available_balance )
+        else if (status == Invaild_value)
         {
-            return Insufficient_cash;
+            setComponentText
+            (TransferGUI, "Title", "<html>Please enter transfer amount!<br/>(0 to cancel)" +
+            "<br/><br/><br/><br/>Only support numbers! Input again</html>"
+            , transferfont);
         }
-        else 
+        else if (status == Insufficient_cash)
         {
-            return 1;
+            setComponentText
+            (TransferGUI, "Title", "<html>Please enter transfer amount!<br/>(0 to cancel)" + 
+            "<br/><br/><br/><br/>Insufficient cash available<br/>Please select smaller amount</html>"
+            , transferfont);
         }
-   }
-   
-   public static int check_account( String input )
-   {    
-        double target_account_temp;
-        try 
+        else
         {
-            target_account_temp = Double.parseDouble( input );
-            account_checker = Integer.parseInt( input );
-        } catch (IllegalArgumentException e) {
-            return Invaild_value;
+            amount = input;
+            setComponentText
+            (TransferGUI, "Title", "<html>Please enter target account number<br/>(0 to cancel)</html>", transferfont);
         }
-        if ( target_account_temp % 1 != 0) 
-        {   
-            return Decimal_value;
-        }
-        else if ( target_account_temp == CANCELED)
+    }
+    
+    private void execute_targetaccount()
+    {
+        String input = getTextPaneText (TransferGUI );
+        int status = Transfer.check_account ( input );
+        if (status == CANCELED)
         {
-            return CANCELED;
+            ATMgui.get().display(GUIType.MainMenu);
         }
-        else if ( account_checker == current_account)
+        else if (status == Decimal_value)
         {
-            return same_account;
+            setComponentText
+            (TransferGUI, "Title", "<html>Please enter transfer amount!<br/>(0 to cancel)" +
+            "<br/><br/><br/><br/>Only support integer value! Input again</html>"
+            , transferfont);
         }
-        else 
+        else if (status == Invaild_value)
         {
-            target_account = Integer.parseInt( input );
-            return 1;
+            setComponentText
+            (TransferGUI, "Title", "<html>Please enter transfer amount!<br/>(0 to cancel)" +
+            "<br/><br/><br/><br/>Only support numbers! Input again</html>"
+            , transferfont);
         }
-   }
-} // end class Transfer
+        else if (status == same_account)
+        {
+            setComponentText
+            (TransferGUI, "Title", "<html>Please enter transfer amount!<br/>(0 to cancel)" + 
+            "<br/><br/><br/><br/>Transfer to your own account<br/>is unavailable" + 
+            "<br/>Please input again</html>"
+            , transferfont);
+        }
+        else if (status == Invaild_account)
+        {
+            setComponentText
+            (TransferGUI, "Title", "<html>Please enter transfer amount!<br/>(0 to cancel)" +
+            "<br/><br/><br/><br/>The account does not exist! Input again</html>"
+            , transferfont);
+        }
+        else
+        {
+            target_account = input;
+            setComponentText
+            (TransferGUI, "Title", "<html>Please enter target account number<br/>(0 to cancel)</html>", transferfont);
+            promt_confirm();
+        }
+    }
+    
+    private void promt_confirm()
+    {   
+        setComponentText
+        (TransferGUI, "Title", "Please check all the inputs are correct", transferfont);
+        for (int i = 2; i<= 7; i++)
+        {
+            setSelectionDisplay(TransferGUI, i, true);
+        }
+        ATMgui.get().setComponentText(TransferGUI, Defaultgui.SELECTION2_LABEL, "transfer amount:", amountfont);
+        ATMgui.get().setComponentText(TransferGUI, Defaultgui.SELECTION3_LABEL, amount);
+        ATMgui.get().setComponentText(TransferGUI, Defaultgui.SELECTION4_LABEL, "target account:", amountfont);
+        ATMgui.get().setComponentText(TransferGUI, Defaultgui.SELECTION5_LABEL, target_account);
+        ATMgui.get().setComponentText(TransferGUI, Defaultgui.SELECTION6_LABEL, "Confirm", amountfont);
+        ATMgui.get().setComponentText(TransferGUI, Defaultgui.SELECTION7_LABEL, "Cancel", amountfont);
+    }
+    
+    private void transfer_execute()
+    {
+        for (int i = 0; i<= 7; i++)
+        {
+            setSelectionDisplay(TransferGUI, i, false);
+        }
+        Transfer.transfer_execute();
+        
+        //message for returning mainmenu after finish transfer
+        for(int i = 0; i < 8 ; i++){
+        ATMgui.get().setSelectionListener(i, new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ATMgui.get().display(GUIType.MainMenu);
+            }
+        });
+        }
+        setComponentText
+        (TransferGUI, "Title", "<html>Transfer successed<br/> " +
+        "Press any key back to main menu</html>", transferfont);
+    }
+    
+    public void setallListener()
+    {
+        ActionListener buttonListener = new ActionListener()
+        
+        {
+                 @Override
+                 public void actionPerformed(ActionEvent e)
+                 {
+                     if (amount == "-1") 
+                     execute_amount();
+                     else if (target_account == "-1")
+                     execute_targetaccount();
+                     
+                     setTextPaneText(TransferGUI, "");
+                 }
+        };
+        
+        ATMgui.get().setEnterListener( buttonListener );
+         
+        ATMgui.get().setSelectionListener(7, new ActionListener() 
+        {
 
+                @Override
+                public void actionPerformed(ActionEvent e) 
+                {
+                    ATMgui.get().display(GUIType.MainMenu);
+                }
+        });
+        ATMgui.get().setSelectionListener(3, new ActionListener() 
+        {
 
-
-/**************************************************************************
- * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
+                @Override
+                public void actionPerformed(ActionEvent e) 
+                {
+                    if (amount != "-1" && target_account !="-1")
+                    transfer_execute();
+                }
+        });
+    }
+}
+    
