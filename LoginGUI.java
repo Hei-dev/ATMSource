204a479aa7d0479adf32b48cb554bf5e7e0abd6d
NOZOs_eduuse <112964915+NOZOs@users.noreply.github.com>
2023-11-24 15:06:10 +0800

    Update LoginGUI.java

--- LoginGUI.java
+++ LoginGUI.java
@@ -10,93 +10,85 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class LoginGUI implements Defaultgui{
-    
+
     private JPanel login;
     private BankDatabase loginBankDB;
-    
-    private String line;
-    
+
     private int currentAccountNumber;
-    
+
     private Font loginFont;
-    
-    private int accountNumber;
-    private int PIN;
-    
+
+    public int accountNumber;
+    public int PIN;
+
     protected LoginGUI(){
         System.out.println("Login");
         login = getdefaultGUI();
         loginFont = new Font("loginFont", 1 ,20);
-	
-        setComponentText(login, Defaultgui.TITLE_LABEL, "Please Enter Your Account Number", loginFont);
-	
-	
+
+        setComponentText(login, "Title", "Please Enter Your Account Number", loginFont);
+
+
         for (int i = 0; i < 8; i++) {
             setSelectionDisplay(login, i, false);
         }
+
         loginBankDB = new BankDatabase();
+
     }
-    
+
     public JPanel getPanel() {
         return login;
     }
-    
+
     // Acc number enter
     public void setAllListener() {
-    // Acc number enter
-        ATMgui.get().setEnterListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent ae) {
-
-            System.out.println("TextPane: "+getTextPaneText(login));
-            accountNumber = Integer.parseInt(getTextPaneText(login));
-            passwordCheck();
-            System.out.println("enter acc number");
-			setTextPaneText(login, "");
-        }
+        // Acc number enter
+        ATMgui.get().setEnterListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+                String accountNumberinput = getTextPaneText (login);
+                setComponentText(login, "Title", accountNumberinput, loginFont);
+                //String input = getTextPaneText( TransferGUI );
+                System.out.println(accountNumberinput);
+                passwordCheck();
+                System.out.println("enter acc number");
+            }
         });
     }
-    
     // Password check
-    public void passwordCheck() {
-	    ATMgui.get().setEnterListener(new ActionListener() 
-	    {
-	        @Override
-	        public void actionPerformed(ActionEvent ae)
-	        {
-	            setComponentText(login, Defaultgui.TITLE_LABEL, "Please Enter Your PIN", loginFont);
-	            ATMgui.get().revalidate();
-	            ATMgui.get().repaint();
-	            
-	            PIN = Integer.parseInt(getTextPaneText(login));
-	            boolean userAuthenticated = loginBankDB.authenticateUser( accountNumber, PIN );
-	            System.out.println("password enter");
-				setTextPaneText(login, "");
-	            // check whether authentication succeeded
-	            if ( userAuthenticated )
-	            {
-	                currentAccountNumber = accountNumber; // save user's account #
-	                ATMgui.get().display(GUIType.MainMenu);
-	                // proceeed to main menu
-	            } 
-	            else 
-	            {
-	                 setComponentText(login, Defaultgui.TITLE_LABEL, "<html>Account Number or PIN wrong,<br />Please Enter Again</html>", loginFont);                
-	                 ATMgui.get().revalidate();
-	                 ATMgui.get().repaint();
-	                
-	                 ATMgui.get().setEnterListener(new ActionListener() 
-	                 {
-	                     @Override
-	                     public void actionPerformed(ActionEvent evt)
-	                     {
-	                        passwordCheck();
-	                        System.out.println("wrong acc or pin");
-	                        setTextPaneText(login, "");
-		                 }
-	                 });
-	            }
-	        }
-	    });
+    public void passwordCheck(){
+        ATMgui.get().setEnterListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent ae){
+
+                setComponentText(login, "Title", "Please Enter Your PIN", loginFont);
+
+                String PINinput = getTextPaneText (login);
+                //PIN = Integer.parseInt(PINinput);
+                boolean userAuthenticated = loginBankDB.authenticateUser( accountNumber, PIN );
+                System.out.println("password enter");
+                // check whether authentication succeeded
+                if ( userAuthenticated )
+                {
+                    currentAccountNumber = accountNumber; // save user's account #
+                    ATMgui.get().display(GUIType.MainMenu);
+                    // proceeed to main menu
+                }
+
+                else{
+
+                    setComponentText(login, "Title", "Account Number or PIN wrong, Please Enter Again", loginFont);
+
+                    ATMgui.get().setEnterListener(new ActionListener(){
+                        @Override
+                        public void actionPerformed(ActionEvent evt){
+                            passwordCheck();
+                            System.out.println("wrong acc or pin");
+                        }
+                    });
+                }
+            }
+        });
     }
 }
